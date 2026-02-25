71886a1a6c9c5a6dc535238dd37ae85924368f07
avalstech <138970146+avalstech@users.noreply.github.com>
2026-02-25 14:07:33 +0000

    debug3

---
+++ .next/dev/server/_ssr_amplify_outputs_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_ssr_amplify_outputs_json";
+exports.ids = ["_ssr_amplify_outputs_json"];
+exports.modules = {
+
+/***/ "(ssr)/./amplify_outputs.json":
+/*!******************************!*\
+  !*** ./amplify_outputs.json ***!
+  \******************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"version":"1","auth":{"user_pool_id":"dummy","user_pool_client_id":"dummy","identity_pool_id":"dummy","aws_region":"us-east-1","authentication_flow_type":"USER_SRP_AUTH","oauth":{}},"data":{"url":"http://localhost:3000/graphql","aws_region":"us-east-1","default_authorization_type":"API_KEY","api_key":"dummy"},"storage":{"aws_region":"us-east-1","bucket_name":"dummy"}}');
+
+/***/ })
+
+};
+;
