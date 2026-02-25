0fa5abc7eafeb01525e72464297438df43be63cb
avalstech <138970146+avalstech@users.noreply.github.com>
2026-02-25 05:08:30 -0600

    Delete rate limit table definition
    Remove rate limit table definition from resource.ts

--- amplify/rate-limit/resource.ts
+++ amplify/rate-limit/resource.ts
@@ -1,13 +1,1 @@
-import { defineStorage } from "@aws-amplify/backend";
-import { aws_dynamodb as dynamodb } from "aws-cdk-lib";
 
-export const rateLimitTable = {
-  resources: {
-    table: new dynamodb.TableV2(undefined as any, "RateLimitTable", {
-      partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
-      sortKey: { name: "sk", type: dynamodb.AttributeType.STRING },
-      timeToLiveAttribute: "ttl",
-      billing: dynamodb.Billing.onDemand()
-    })
-  }
-} as any;
