ed3806f894395114d4c9580d751f4f96a9898dcc
avalstech <138970146+avalstech@users.noreply.github.com>
2026-02-25 05:00:21 -0600

    Refactor backend configuration by removing rate limit table
    Removed rate limit table from backend configuration and adjusted environment variables accordingly.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,18 +3,10 @@ import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { storage } from "./storage/resource";
 import { outboxProcessor } from "./functions/outbox-processor/resource";
-import { rateLimitTable } from "./rate-limit/resource";
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
   storage,
-  outboxProcessor,
-  rateLimitTable
+  outboxProcessor
 });
-
-// Permissions: outbox processor can read/write data and send emails + access rate limit table
-backend.outboxProcessor.resources.lambda.addEnvironment("SITE_URL", process.env.SITE_URL ?? "http://localhost:3000");
-backend.outboxProcessor.resources.lambda.addEnvironment("RATE_LIMIT_TABLE", backend.rateLimitTable.resources.table.tableName);
-
-backend.data.resources.cfnResources.cfnGraphqlApi.addDependency(backend.outboxProcessor.resources.lambda);
